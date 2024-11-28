917fd9e26ed9c5e3992f330e8ecab336ad1375c4
Chralu <contact@charly-caulet.net>
2024-11-28 15:37:12 +0100

    feat: :sparkles: Integrated dapp opens external websites in default browser.

--- lib/infrastructure/rpc/awc_webview.dart
+++ lib/infrastructure/rpc/awc_webview.dart
@@ -179,7 +179,9 @@ class _AWCWebviewState extends State<AWCWebview> with WidgetsBindingObserver {
                     return NavigationActionPolicy.CANCEL;
                   }
 
-                  if (navigationAction.request.method == 'GET' &&
+                  if ((navigationAction.navigationType == null ||
+                          navigationAction.navigationType ==
+                              NavigationType.LINK_ACTIVATED) &&
                       (uri.scheme != widget.uri.scheme ||
                           uri.host != widget.uri.host)) {
                     await _openThirdPartyWebsite(uri);
