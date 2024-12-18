ba16800fa88ee561d1000906fdf7b85333db17ad
Chralu <contact@charly-caulet.net>
2024-12-18 22:10:43 +0100

    refactor: :recycle: Use NoTransitionPage in router.

--- lib/router/router.introduction.dart
+++ lib/router/router.introduction.dart
@@ -3,93 +3,51 @@ part of 'router.dart';
 final _introductionRoutes = [
   GoRoute(
     path: IntroConfigureSecurity.routerPage,
-    pageBuilder: (context, state) => CustomTransitionPage<void>(
-      transitionDuration: Duration.zero,
-      reverseTransitionDuration: Duration.zero,
+    pageBuilder: (context, state) => NoTransitionPage<void>(
       key: state.pageKey,
       child: IntroConfigureSecurity(
         isImportProfile:
             (state.extra! as Map<String, dynamic>)['isImportProfile']! as bool,
       ),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-          FadeTransition(
-        opacity: animation,
-        child: child,
-      ),
     ),
   ),
   GoRoute(
     path: IntroWelcome.routerPage,
-    pageBuilder: (context, state) => CustomTransitionPage<void>(
-      transitionDuration: Duration.zero,
-      reverseTransitionDuration: Duration.zero,
+    pageBuilder: (context, state) => NoTransitionPage<void>(
       key: state.pageKey,
       child: const IntroWelcome(),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-          FadeTransition(
-        opacity: animation,
-        child: child,
-      ),
     ),
   ),
   GoRoute(
     path: IntroNewWalletGetFirstInfos.routerPage,
-    pageBuilder: (context, state) => CustomTransitionPage<void>(
-      transitionDuration: Duration.zero,
-      reverseTransitionDuration: Duration.zero,
+    pageBuilder: (context, state) => NoTransitionPage<void>(
       key: state.pageKey,
       child: const IntroNewWalletGetFirstInfos(),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-          FadeTransition(
-        opacity: animation,
-        child: child,
-      ),
     ),
   ),
   GoRoute(
     path: IntroBackupSeedPage.routerPage,
-    pageBuilder: (context, state) => CustomTransitionPage<void>(
-      transitionDuration: Duration.zero,
-      reverseTransitionDuration: Duration.zero,
+    pageBuilder: (context, state) => NoTransitionPage<void>(
       key: state.pageKey,
       child: IntroBackupSeedPage(
         name: state.extra! as String,
-      ),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-          FadeTransition(
-        opacity: animation,
-        child: child,
       ),
     ),
   ),
   GoRoute(
     path: IntroNewWalletDisclaimer.routerPage,
-    pageBuilder: (context, state) => CustomTransitionPage<void>(
-      transitionDuration: Duration.zero,
-      reverseTransitionDuration: Duration.zero,
+    pageBuilder: (context, state) => NoTransitionPage<void>(
       key: state.pageKey,
       child: IntroNewWalletDisclaimer(
         name: state.extra! as String,
-      ),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-          FadeTransition(
-        opacity: animation,
-        child: child,
       ),
     ),
   ),
   GoRoute(
     path: IntroImportSeedPage.routerPage,
-    pageBuilder: (context, state) => CustomTransitionPage<void>(
-      transitionDuration: Duration.zero,
-      reverseTransitionDuration: Duration.zero,
+    pageBuilder: (context, state) => NoTransitionPage<void>(
       key: state.pageKey,
       child: const IntroImportSeedPage(),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-          FadeTransition(
-        opacity: animation,
-        child: child,
-      ),
     ),
   ),
   GoRoute(
